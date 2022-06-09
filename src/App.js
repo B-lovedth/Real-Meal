3b9cd7df3489a2661f29bb226634f6093f158a20
Great Solomon <greatsoma2019@gmail.com>
2022-06-09 16:15:13 +0100

    initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+// import {BrowserRouter as Router,Route,Switch } from 'react-router-dom'
+
+
+
+function App() {
+  return (
+    <div className="App">
+      <h1>hello world</h1>
+    </div>
+    );
+}
+
+export default App;
---
+++ src/components/Popular.jsx
@@ -0,0 +1,9 @@
+
+const Popular = () => {
+  return (
+    <div>
+    </div>
+  )
+}
+
+export default Popular
