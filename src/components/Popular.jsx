a72a484202d3dcb466a77d28636d3da71a5d9c68
Great Solomon <greatsoma2019@gmail.com>
2022-06-11 21:00:22 +0100

    usb commit

--- src/components/Popular.jsx
+++ src/components/Popular.jsx
@@ -9,7 +9,7 @@ const Popular = () => {
   const getPopular = async () => {
       const api = await fetch(`https://api.spoonacular.com/recipes/random?apiKey=${process.env.MY_API_KEY}&number=9`)
       const data = await api.json()
-      console.log(data)
+      console.log(data) 
     }
   return (
     <div>
