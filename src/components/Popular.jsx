502539e75befb8a9f3f11695b5322ea0cd0c44d8
Great Solomon <greatsoma2019@gmail.com>
2022-06-11 21:19:16 +0100

    sub commit

--- src/components/Popular.jsx
+++ src/components/Popular.jsx
@@ -1,7 +1,7 @@
-import { useEffect } from "react"
+import { useEffect, useState } from "react"
 
 const Popular = () => {
-
+  const [popular,setPopular] = useState([])
   useEffect(() => {
     getPopular()
   }, [])
