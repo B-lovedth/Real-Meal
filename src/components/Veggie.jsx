8ab7b318ab40f6d63a2124ccf352ffc134d38bc3
Great Solomon <greatsoma2019@gmail.com>
2022-06-25 00:02:49 +0100

    Update Veggie.jsx

--- src/components/Veggie.jsx
+++ src/components/Veggie.jsx
@@ -2,8 +2,31 @@ import React from 'react'
 
 const Veggie = () => {
   return (
-    <div>
-      <h1>Veggie</h1>
+     <div>
+      <Wrapper>
+        <h3>Popular Picks</h3>
+        <Splide
+          options={{
+            perPage: 3,
+            arrows: false,
+            pargination: false,
+            drag: 'free',
+            gap: '5rem',
+          }}>
+          {popular.map((recipe) => {
+            return (
+              <SplideSlide key={recipe.id}>
+                <Card>
+                  <p>{recipe.title}</p>
+                  <img src={recipe.image} alt={recipe.title} />
+                  <Gradient />
+                </Card>
+              </SplideSlide>
+            )
+          }
+          )}
+        </Splide>
+      </Wrapper>
     </div>
   )
 }
