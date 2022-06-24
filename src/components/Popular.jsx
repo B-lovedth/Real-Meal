13bde6b28b1abc2968c6ce191e1fb6cb1aaa5c4c
Great Solomon <greatsoma2019@gmail.com>
2022-06-25 00:29:55 +0100

    Update Popular.jsx

--- src/components/Popular.jsx
+++ src/components/Popular.jsx
@@ -27,9 +27,6 @@ const Popular = () => {
       })   
       console.log(popular)
     }
-      
-      
-   
   }
   return (
     <div>
@@ -63,39 +60,38 @@ const Popular = () => {
 
 const Wrapper = styled.div`
   margin: 4rem 0rem;
-`;
-
+  `;
 const Card = styled.div`
   min-height: 25rem;
   border-radius: 2rem;
   overflow : hidden;
   position:relative;
-  img{
-    border-radius:2rem;
-    position:absolute;
-    left:0;
-    width:100%;
-    height:100%;
-    object-fit:cover;
-  }
-  p{
-    position:absolute;
-    z-index:10;
-    left:50%;
-    bottom:0%;
-    transform: translate(-50%,0%);
-    color: white;
-    width:100%;
-    text-align: center;
-    font-weight:600;
+        img{
+            border-radius:2rem;
+            position:absolute;
+            left:0;
+            width:100%;
+            height:100%;
+            object-fit:cover;
+        }
+        p{
+            position:absolute;
+            z-index:10;
+            left:50%;
+            bottom:0%;
+            transform: translate(-50%,0%);
+            color: white;
+            width:100%;
+            text-align: center;
+            font-weight:600;
 
-    font-size:1.5rem;
-    height:40%;
-    display:flex;
-    justify-content: center;
-    align-items:center;
-  }
-`;
+            font-size:1.5rem;
+            height:40%;
+            display:flex;
+            justify-content: center;
+            align-items:center;
+        }
+        `;
 
 const Gradient = styled.div`
   z-index: 3;
@@ -103,5 +99,6 @@ const Gradient = styled.div`
   width:100%;
   height : 100%;
   background:linear-gradient(rgba(0,0,0,0) , rgba(0,0,0,0.5))
-`;
+    `;
+
 export default Popular
