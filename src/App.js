e6529d864c8bc0e09ea1d7a3b2298cfafcd8c345
Great Solomon <greatsoma2019@gmail.com>
2022-07-02 20:11:53 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
-// import Category from "./components/Category";
+import { BrowserRouter } from "react-router-dom";
+import Category from "./components/Category";
 import Pages from "./pages/Pages";
 // import './bootstrap1.css'
 
 function App() {
   return (
     <div className="App">
-      {/* <Category/> */}
-      <Pages/>
+      <BrowserRouter>
+        <Category/>
+        <Pages />
+      </BrowserRouter>
     </div>
     );
 }
