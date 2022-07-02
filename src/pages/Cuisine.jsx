abfce210216f3d5ccf923983e89d1f526b124b9c
Great Solomon <greatsoma2019@gmail.com>
2022-07-02 21:50:57 +0100

    Update Cuisine.jsx

--- src/pages/Cuisine.jsx
+++ src/pages/Cuisine.jsx
@@ -1,7 +1,26 @@
-import React from 'react'
+import { motion } from "framer-motion";
+import { useEffect, useState } from "react";
+import { Link , useParams } from "react-router-dom";
+import styled from "styled-components";
+
+
 
 function Cuisine() {
-  return (
+    const [cuisine, setCuisine] = useState([])
+    useEffect(() => {
+        getCuisine()
+    })
+    
+    const getCuisine = (name) => {
+        fetch(`https://api.spoonacular.com/recipes/complexSearch?apiKey=${process.env.REACT_APP_API_KEY}&cuisine={name}`)
+            .then((res) => res.json())
+            .then((data) => {
+                setCuisine(data.recipe)
+                console.log(data)
+            })
+    }
+
+    return (
     <div>
       Cuisine
     </div>
