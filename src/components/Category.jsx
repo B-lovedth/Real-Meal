6346495bf54d6254d291d92d158d0306892c4939
Great Solomon <greatsoma2019@gmail.com>
2022-07-10 22:17:53 +0100

    Update Category.jsx

--- src/components/Category.jsx
+++ src/components/Category.jsx
@@ -7,19 +7,19 @@ import styled from 'styled-components'
 const Category = () => {
     return (
         <List>
-            <NavLink to={"/cuisine/Italian"}>
+            <NavLink to="/cuisine/Italian">
                 <FaPizzaSlice />
                 <h4>Italian</h4>
             </NavLink>
-            <NavLink to={"/cuisine/American"}>
+            <NavLink to="/cuisine/American">
                 <FaHamburger />
                 <h4>American</h4>
             </NavLink>
-            <NavLink to={"/cuisine/Thai"}>
+            <NavLink to="/cuisine/Thai">
                 <GiNoodles />
                 <h4>Thai</h4>
             </NavLink>
-            <NavLink to={"/cuisine/Japanese"}>
+            <NavLink to="/cuisine/Japanese">
                 <GiChopsticks />
                 <h4>Japanese</h4>
             </NavLink>
