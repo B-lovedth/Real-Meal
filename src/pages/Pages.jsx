1071b256b4df2ec9d9e8a25dd0dec4fec2175ffc
B-lovedth <greatsoma2019@gmail.com>
2022-07-12 21:02:13 +0100

    Testing Pages.jsx

--- src/pages/Pages.jsx
+++ src/pages/Pages.jsx
@@ -1,10 +1,12 @@
 import { Route, Routes } from 'react-router-dom';
 import Cuisine from './Cuisine';
 import Home from './Home';
+import LandingPage from './LandingPage';
   
 const Pages = () => {
   return (
     <div>
+      <LandingPage/>
       <Routes>
           <Route exact path="/" element={<Home/>}/>
           <Route path="/cuisine/:type" element={<Cuisine/>} />
