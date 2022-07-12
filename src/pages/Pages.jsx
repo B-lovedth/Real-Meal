ca1a97fa44dab02b26ce13c407d9e91a013230d9
B-lovedth <greatsoma2019@gmail.com>
2022-07-12 21:35:20 +0100

    Update Pages.jsx

--- src/pages/Pages.jsx
+++ src/pages/Pages.jsx
@@ -1,12 +1,10 @@
 import { Route, Routes } from 'react-router-dom';
 import Cuisine from './Cuisine';
 import Home from './Home';
-import LandingPage from '../components/LandingPage';
   
 const Pages = () => {
   return (
     <div>
-      <LandingPage/>
       <Routes>
           <Route exact path="/" element={<Home/>}/>
           <Route path="/cuisine/:type" element={<Cuisine/>} />
