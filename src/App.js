c195971764a30f7879ad67e28b43c8aea5fad3b4
B-lovedth <greatsoma2019@gmail.com>
2022-07-14 21:13:01 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import { BrowserRouter } from "react-router-dom";
 import Category from "./components/Category";
+import Navbar from './Navbar'
 import LandingPage from "./components/LandingPage";
 import Pages from "./pages/Pages";
 
 function App() {
   return (
     <div className="App">
-      
+      <Navbar/>
       <BrowserRouter>
         <LandingPage/>
         <Category/>
