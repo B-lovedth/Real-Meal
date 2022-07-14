73e849253b64fb792a528f94c8ffe3647699dd24
B-lovedth <greatsoma2019@gmail.com>
2022-07-14 21:13:10 +0100

    Create Navbar.jsx

---
+++ src/Navbar.jsx
@@ -0,0 +1,81 @@
+import { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+import "./Navbar.css";
+
+const Navbar = () => {
+  const [click, setClick] = useState(false);
+  const [button, setButton] = useState(true);
+  const handleClick = () => {
+    setClick(!click);
+  };
+
+  const closeMobileMenu = () => {
+    setClick(false);
+  };
+
+  const showButton = () => {
+    if (window.innerWidth <= 960) {
+      setButton(false);
+    } else {
+      setButton(true);
+    }
+  };
+
+  window.addEventListener("resize", showButton);
+
+  useEffect(() => {
+    showButton();
+  }, []);
+
+  return (
+    <div>
+      <nav className="navbar">
+        <div className="navbar-container">
+          <Link to="/" className="navbar-logo" onClick={closeMobileMenu}>
+            <i class="fa-brands fa-sitrox fa-bounce"></i>OMA
+          </Link>
+          <div className="menu-icon" onClick={handleClick}>
+            <i className={click ? "fas fa-times" : "fas fa-bars-staggered"}> </i>
+          </div>
+          <ul className={click ? "nav-menu active" : "nav-menu"}>
+            <li className="nav-item">
+              <Link to="/" className="nav-links" onClick={closeMobileMenu}>
+                {click ? "Home" : <i class="fa-solid fa-house"></i>}
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link
+                to="/services"
+                className="nav-links"
+                onClick={closeMobileMenu}
+              >
+                {click ? "Services" : <i class="fa-solid fa-computer"></i>}
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link
+                to="/products"
+                className="nav-links"
+                onClick={closeMobileMenu}
+              >
+                {click ? "Products" : <i class="fa-solid fa-bars-progress"></i>}
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link
+                to="/sign-up"
+                className="nav-links-mobile"
+                onClick={closeMobileMenu}
+              >
+                Sign-Up
+              </Link>
+            </li>
+          </ul>
+          {/* {button && <Button buttonStyle="btn--outline" link='/sign-up'>SIGN UP</Button>} */}
+        </div>
+      </nav>
+    </div>
+  );
+};
+
+export default Navbar;
