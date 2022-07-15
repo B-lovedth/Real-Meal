69696f64220ad8e682dcb2136e32caaaf2f8ff0b
B-lovedth <greatsoma2019@gmail.com>
2022-07-15 05:19:06 +0100

    Sub commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import Pages from "./pages/Pages";
 function App() {
   return (
     <div className="App">
-      <Navbar/>
+      {/* <Navbar/> */}
       <BrowserRouter>
         <LandingPage/>
         <Category/>
