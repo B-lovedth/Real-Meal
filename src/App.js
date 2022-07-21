ea23df615acc796fb43b0be81b6ad0b27e43f8bb
B-lovedth <greatsoma2019@gmail.com>
2022-07-21 12:33:55 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { BrowserRouter } from "react-router-dom";
 import Category from "./components/Category";
 import Navbar from './Navbar'
 import Pages from "./Pages";
+import styled from 'styled-components'
 
 
 function App() {
