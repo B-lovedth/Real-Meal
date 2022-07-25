c0f0ec86bd039487d9a85b5b6e967f129f8e5c1a
B-lovedth <greatsoma2019@gmail.com>
2022-07-25 10:06:58 +0100

    Update Category.jsx

--- src/components/Category.jsx
+++ src/components/Category.jsx
@@ -30,9 +30,45 @@ const Category = () => {
 const List = styled.div`
     display:flex;
     justify-content: center;
-    margin:1rem 0rem;
+    margin:0rem;
     h4{
         font-size:0.7rem;
+    }
+`
+const SLink = styled(NavLink)`
+    display: flex;
+    flex-direction: column;
+    justify-content:center;
+    align-items:center;
+    border-radius:50%;
+    margin-right:2rem;
+    text-decoration:none;
+    background: linear-gradient(35deg, #5c5b5b , #313131);
+    width:4rem;
+    height:4rem;
+    cursor:pointer;
+    transform: scale(0.8);
+
+    h4{
+        color:white;
+        font-size:0.7rem;
+    }
+    svg{
+        color:white;
+        font-size:1.5rem;
+    }&.active{
+        background: linear-gradient(to right, #21f239, #a3e940 );
+    }
+    @media(max-width:1200px) {
+        width: 3rem;
+        height: 3rem;
+        h4{
+            font-size:0.55rem;
+            font-weight:600;
+        }
+        svg{
+            font-size:1rem;
+        }
     }
 `
 export default Category
