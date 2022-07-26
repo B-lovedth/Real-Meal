abdb1748a1ebf3c7d9d40a6ff9eccc04bd7378ad
B-lovedth <greatsoma2019@gmail.com>
2022-07-26 09:59:31 +0100

    Update Veggie.jsx

--- src/components/Veggie.jsx
+++ src/components/Veggie.jsx
@@ -120,10 +120,10 @@ const Card = styled.div`
 const Gradient = styled.div`
   z-index: 3;
   position: absolute;
-  width:100%;
-  height : 100%;
-  background:linear-gradient(rgba(0,0,0,0) , rgba(0,0,0,0.5))
-    `;
+  width: 100%;
+  height: 100%;
+  background: linear-gradient(rgba(0, 0, 0, 0), rgba(0, 0, 0, 0.5));
+`;
 
 const P = styled.p`
             position:absolute;
