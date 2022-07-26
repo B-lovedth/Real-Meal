e05d57c7461237a4ce06ec2059975411a3e5eec7
B-lovedth <greatsoma2019@gmail.com>
2022-07-26 10:00:04 +0100

    Update Pages.jsx

--- src/Pages.jsx
+++ src/Pages.jsx
@@ -2,6 +2,7 @@ import { Route, Routes } from 'react-router-dom';
 import Contact from './pages/Contact';
 import Cuisine from './pages/Cuisine';
 import Home from './pages/Home';
+import SCuisine from './pages/SCuisine';
   
 const Pages = () => {
   return (
@@ -10,6 +11,7 @@ const Pages = () => {
         <Route exact path="/" element={<Home/>}/>
         <Route path="/cuisine/:type" element={<Cuisine />} />
         <Route exact path="/contact" element={ <Contact/>} />
+        <Route exact path='/cuisines' element={<SCuisine/>}/>
       </Routes>
     </div>
   )
