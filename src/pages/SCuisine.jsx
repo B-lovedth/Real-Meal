042f857fc955973a317409a91fb8f091449324cc
B-lovedth <greatsoma2019@gmail.com>
2022-07-30 09:42:25 +0100

    Update SCuisine.jsx

--- src/pages/SCuisine.jsx
+++ src/pages/SCuisine.jsx
@@ -3,38 +3,43 @@ import styled from 'styled-components';
 
 const SCuisine = () => {
   return (
-    <Grid>
-      <StyledLink to='/cuisine/African'>African</StyledLink>
-      <StyledLink to='/cuisine/American'>American</StyledLink>
-      <StyledLink to='/cuisine/British'>British</StyledLink>
-      <StyledLink to='/cuisine/Cajun'>Cajun</StyledLink>
-      <StyledLink to='/cuisine/Caribbean'>Caribbean</StyledLink>
-      <StyledLink to='/cuisine/European'>European</StyledLink>
-      <StyledLink to='/cuisine/French'>French</StyledLink>
-      <StyledLink to='/cuisine/German'>German</StyledLink>
-      <StyledLink to='/cuisine/Greek'>Greek</StyledLink>
-      <StyledLink to='/cuisine/Indian'>Indian</StyledLink>
-      <StyledLink to='/cuisine/Irish'>Irish</StyledLink>
-      <StyledLink to='/cuisine/Italian'>Italian</StyledLink>
-      <StyledLink to='/cuisine/Japanese'>Japanese</StyledLink>
-      <StyledLink to='/cuisine/Jewish'>Jewish</StyledLink>
-      <StyledLink to='/cuisine/Korean'>Korean</StyledLink>
-      <StyledLink to='/cuisine/Latin American'>Latin American</StyledLink>
-      <StyledLink to='/cuisine/Mediterranean'>Mediterranean</StyledLink>
-      <StyledLink to='/cuisine/Mexican'>Mexican</StyledLink>
-      <StyledLink to='/cuisine/Middle Eastern'>Middle Eastern</StyledLink>
-      <StyledLink to='/cuisine/Nordic'>Nordic</StyledLink>
-      <StyledLink to='/cuisine/Southern'>Southern</StyledLink>
-      <StyledLink to='/cuisine/Spanish'>Spanish</StyledLink>
-      <StyledLink to='/cuisine/Thai'>Thai</StyledLink>
-      <StyledLink to='/cuisine/Vietnamese'>Vietnamese</StyledLink>
-
-    </Grid>
-  )
+    <Sdiv >
+      <Grid>
+        <StyledLink to='/cuisine/African'>African</StyledLink>
+        <StyledLink to='/cuisine/American'>American</StyledLink>
+        <StyledLink to='/cuisine/British'>British</StyledLink>
+        <StyledLink to='/cuisine/Cajun'>Cajun</StyledLink>
+        <StyledLink to='/cuisine/Caribbean'>Caribbean</StyledLink>
+        <StyledLink to='/cuisine/European'>European</StyledLink>
+        <StyledLink to='/cuisine/French'>French</StyledLink>
+        <StyledLink to='/cuisine/German'>German</StyledLink>
+        <StyledLink to='/cuisine/Greek'>Greek</StyledLink>
+        <StyledLink to='/cuisine/Indian'>Indian</StyledLink>
+        <StyledLink to='/cuisine/Irish'>Irish</StyledLink>
+        <StyledLink to='/cuisine/Italian'>Italian</StyledLink>
+        <StyledLink to='/cuisine/Japanese'>Japanese</StyledLink>
+        <StyledLink to='/cuisine/Jewish'>Jewish</StyledLink>
+        <StyledLink to='/cuisine/Korean'>Korean</StyledLink>
+        <StyledLink to='/cuisine/Latin American'>Latin American</StyledLink>
+        <StyledLink to='/cuisine/Mediterranean'>Mediterranean</StyledLink>
+        <StyledLink to='/cuisine/Mexican'>Mexican</StyledLink>
+        <StyledLink to='/cuisine/Middle Eastern'>Middle Eastern</StyledLink>
+        <StyledLink to='/cuisine/Nordic'>Nordic</StyledLink>
+        <StyledLink to='/cuisine/Southern'>Southern</StyledLink>
+        <StyledLink to='/cuisine/Spanish'>Spanish</StyledLink>
+        <StyledLink to='/cuisine/Thai'>Thai</StyledLink>
+        <StyledLink to='/cuisine/Vietnamese'>Vietnamese</StyledLink>
+      </Grid>
+    </Sdiv>
+  );
 }
-
+const Sdiv = styled.div`
+  height:100%;
+  margin:0;
+`
 const Grid = styled.div`
   display: grid;
+  height:100%;
   grid-template-columns: repeat(auto-fit, minmax(10rem, 1fr));
   grid-gap: 1.5rem;
   margin: 4rem 3rem;
@@ -42,16 +47,16 @@ const Grid = styled.div`
 `;
 
 const StyledLink = styled(Link)`
-  font-weight:600;  
-  padding:1rem 3rem;
-  text-decoration:none;
-  border-radius:18px;
-  &:hover{
-    color:#00df34;
+  font-weight: 600;
+  padding: 1rem 3rem;
+  text-decoration: none;
+  border-radius: 18px;
+  &:hover {
+    color: #e45710;
   }
-  &:active{
-    color:white;
-    background: linear-gradient(to right, #21f239, #a3e940);
+  &:active {
+    color: white;
+    background: linear-gradient(to right, #ffa31d, #ef5454);
   }
-`
+`;
 export default SCuisine
