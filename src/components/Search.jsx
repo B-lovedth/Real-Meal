e26b451b2e3379fe52444689782309dcacb6e0b1
B-lovedth <greatsoma2019@gmail.com>
2022-07-30 13:19:40 +0100

    Update Search.jsx

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -10,31 +10,32 @@ const Search = () => {
 }
 
 const StyleForm = styled.form`
-    position: relative;
+  position: relative;
+  width: 100%;
+  input {
+    border: none;
+    background: #f3f3f3;
+    font-size: 0.75rem;
+    font-weight:600;
+    color: #1a1a1a;
+    letter-spacing:0.5px;
+    padding: 0.5rem 2.5rem;
+    border: none;
+    border-radius: 1rem;
+    outline: none;
+  }
+  svg {
+    position: absolute;
+    top: 50%;
+    left: 0%;
+    transform: translate(100%, -50%);
+  }
+  @media (max-width: 1200px) {
+    margin: 0 auto;
     width: 100%;
-    input{
-        border:none;
-        /* background: linear-gradient(35deg, #494949 , #313131); */
-        background: #fff;
-        font-size:0.75rem;
-        color:white;
-        padding:0.5rem 2.5rem;
-        border:none;
-        border-radius:1rem;
-        outline:none;
+    input {
+      width: 100%;
     }
-    svg{
-        position: absolute;
-        top:50%;
-        left:0%;
-        transform:translate(100%, -50%);
-    }
-    @media(max-width:1200px){
-        margin :0 auto;
-        width:100%;
-        input{
-            width:100%;
-        }
-    }
-`
+  }
+`;
 export default Search
