1a44c904ec08a0f9fe98ba0aa54f1322af29ccdf
B-lovedth <greatsoma2019@gmail.com>
2022-08-01 11:54:39 +0100

    Update Search.jsx

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -4,18 +4,46 @@ import { FaSearch } from 'react-icons/fa'
 import {useNavigate} from 'react-router-dom'
 const Search = () => {
   const [input, setInput] = useState('')
+  const [error , setError] = useState('')
   const navigate = useNavigate()
   const submitHandler = (e) => {
-    e.preventDefault()
-    navigate(`/searched/${input}`)
+    if (input === "") {
+      e.preventDefault();
+      setError("No input");
+    } else if (!isNaN(input)) {
+      e.preventDefault();
+      setError("invalid input");
+    } else {
+      e.preventDefault();
+      setError('')
+      navigate(`/searched/${input}`);
+    } 
   }
   return (
-    <StyleForm onSubmit={submitHandler}>
-      <FaSearch/>
-      <input type="text" onChange={(e) => setInput(e.target.value)} value={input} placeholder="search" />
-    </StyleForm>
-  )
+    <div>
+      <StyleForm onSubmit={submitHandler}>
+        <Button type='submit'>
+          <FaSearch />
+        </Button>
+        <input
+          type='text'
+          onChange={(e) => setInput(e.target.value)}
+          value={input}
+          placeholder='search'
+        />
+      </StyleForm>
+      <Error>{error}</Error>
+    </div>
+  );
 }
+
+const Error = styled.h6`
+  text-align: center;
+  color:red;
+  top:0;
+  margin-top:5px;
+  font-weight:lighter;
+`
 
 const StyleForm = styled.form`
   position: relative;
@@ -32,18 +60,27 @@ const StyleForm = styled.form`
     border-radius: 1rem;
     outline: none;
   }
-  svg {
-    position: absolute;
-    top: 50%;
-    left: 0%;
-    transform: translate(100%, -50%);
-  }
   @media (max-width: 1200px) {
     margin: 0 auto;
     width: 100%;
     input {
       width: 100%;
     }
+  }
+`;
+const Button = styled.button`
+  position: absolute;
+  top: 50%;
+  left: 0%;
+  transform: translate(100%, -50%);
+  border: none;
+  cursor:pointer;
+  svg {
+    position: absolute;
+    top: 50%;
+    left: 0%;
+    transform: translate(100%, -50%);
+    color: #333;
   }
 `;
 export default Search
