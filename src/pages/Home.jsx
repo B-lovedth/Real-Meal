7c70ce8b92890db94b5673beab90335d84a1fa24
B-lovedth <greatsoma2019@gmail.com>
2022-08-03 16:34:55 +0100

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,7 +6,12 @@ import {motion} from 'framer-motion'
 
 const Home = () => {
   return (
-    <motion.div>
+    <motion.div
+      animate={{ opacity: 1 }}
+      initial={{ opacity: 0 }}
+      exit={{ opacity: 0 }}
+      transition={{duration: 0.5}}
+    >
         <HeroSection/>
         <Popular/>
         <Veggie/>
