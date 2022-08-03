be8ba4643dae21f057ecff6368be619f9faa4eea
B-lovedth <greatsoma2019@gmail.com>
2022-08-03 16:34:51 +0100

    Update Recipe.jsx

--- src/pages/Recipe.jsx
+++ src/pages/Recipe.jsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import { useEffect } from "react";
 import { useParams } from "react-router-dom";
 import styled from "styled-components";
+import {motion} from 'framer-motion'
 
 const Recipe = () => {
   const [isPending, setIsPending] = useState(true);
@@ -33,12 +34,17 @@ const Recipe = () => {
     return () => abortCont.abort();
   };
   return (
-    <>
+    <Container>
       {isPending && <h3>Loading..</h3>}
       {error && <h3>{error}</h3>}
       {details && (
-        <DetailWrapper>
-          <div>
+        <DetailWrapper
+          animate={{ opacity: 1 }}
+          initial={{ opacity: 0 }}
+          exit={{ opacity: 0 }}
+          transition={{ duration: 0.5 }}
+        >
+          <div className="left">
             <h2>{details.title}</h2>
             <img src={details.image} alt={details.title} />
           </div>
@@ -62,44 +68,75 @@ const Recipe = () => {
               Ingredients
             </Button>
             {activeTab === "summary" && (
-                <div>
-                  <h3 dangerouslySetInnerHTML={{ __html: details.summary }}></h3>
-                </div>
-              )}
+              <motion.div
+                animate={{ opacity: 1 }}
+                initial={{ opacity: 0 }}
+                exit={{ opacity: 0 }}
+                transition={{ duration: 0.5 }}
+              >
+                <h3 dangerouslySetInnerHTML={{ __html: details.summary }}></h3>
+              </motion.div>
+            )}
             {activeTab === "instructions" && (
-              <div>
-                <h3 dangerouslySetInnerHTML={{ __html: details.instructions }}></h3>
-              </div>
+              <motion.div
+                animate={{ opacity: 1 }}
+                initial={{ opacity: 0 }}
+                exit={{ opacity: 0 }}
+                transition={{ duration: 0.5 }}
+              >
+                <h3
+                  dangerouslySetInnerHTML={{ __html: details.instructions }}
+                ></h3>
+              </motion.div>
             )}
             {activeTab === "ingredients" && (
-              <ul>
+              <motion.ul
+                animate={{ opacity: 1 }}
+                initial={{ opacity: 0 }}
+                exit={{ opacity: 0 }}
+                transition={{ duration: 0.5 }}
+              >
                 {details.extendedIngredients.map((ingredients) => {
                   return <li key={ingredients.id}>{ingredients.original}</li>;
                 })}
-              </ul>
+              </motion.ul>
             )}
           </Info>
         </DetailWrapper>
       )}
-    </>
+    </Container>
   );
 };
-
-const DetailWrapper = styled.div`
+const Container = styled.div`
+  width:100%;
+`
+const DetailWrapper = styled(motion.div)`
+  width: 80%;
+  margin-left: auto;
+  margin-right: auto;
   margin-top: 5rem;
   margin-bottom: 2.5rem;
   display: flex;
+  .left {
+    width: 40%;
+    img {
+      width: 25rem;
+      border-radius: 18px;
+      box-shadow: -5px 8px 5px #504f4fe6;
+    }
+  }
   .active {
     background: linear-gradient(to right, #f66117, #ef5454);
     color: white;
   }
   h2 {
+    text-align:center;
     margin-bottom: 1rem;
-    font-family: 'Lobster Two';
+    font-family: "Lobster Two";
   }
-  h3{
-    font-size:1rem;
-    line-height:1.7rem;
+  h3 {
+    font-size: 1rem;
+    line-height: 1.7rem;
   }
   li {
     font-size: 1rem;
@@ -113,9 +150,11 @@ const DetailWrapper = styled.div`
 const Button = styled.button`
   padding: 1rem 2rem;
   color: #313131;
-  cursor:pointer;
+  cursor: pointer;
   background: white;
-  border: 2px solid black;
+  border: 2px solid transparent;
+  border-radius: 18px;
+  box-shadow: -5px 8px 5px #504f4fe6;
   margin-right: 1rem;
   font-weight: 600;
 `;
