a9c3b93c7c423902316d2a9bd1951ee9c18c4dc5
B-lovedth <greatsoma2019@gmail.com>
2022-08-04 03:31:34 +0100

    Update Pages.jsx

--- src/Pages.jsx
+++ src/Pages.jsx
@@ -5,20 +5,25 @@ import Home from './pages/Home';
 import Recipe from './pages/Recipe';
 import SCuisine from './pages/SCuisine';
 import Searched from './pages/Searched';
+import {AnimatePresence} from 'framer-motion'
+import { useLocation } from 'react-router-dom';
   
 const Pages = () => {
+  const location = useLocation()
   return (
     <div>
-      <Routes>
-        <Route exact path="/" element={<Home/>}/>
-        <Route path="/cuisine/:type" element={<Cuisine />} />
-        <Route exact path="/contact" element={ <Contact/>} />
-        <Route exact path='/cuisines' element={<SCuisine />} />
-        <Route path='/searched/:search' element={<Searched />} />
-        <Route path='/recipe/:name' element={<Recipe/>}/>
-      </Routes>
+      <AnimatePresence exitBeforeEnter>
+        <Routes location={location} key={location.pathname}>
+          <Route exact path='/' element={<Home />} />
+          <Route path='/cuisine/:type' element={<Cuisine />} />
+          <Route exact path='/contact' element={<Contact />} />
+          <Route exact path='/cuisines' element={<SCuisine />} />
+          <Route path='/searched/:search' element={<Searched />} />
+          <Route path='/recipe/:name' element={<Recipe />} />
+        </Routes>
+      </AnimatePresence>
     </div>
-  )
+  );
 }
 
 export default Pages
