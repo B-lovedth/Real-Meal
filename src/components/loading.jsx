89acdf7154620625f1d9fc1029629ef00981e60f
Solomon Great <greatsoma2019@gmail.com>
2022-12-21 19:20:58 +0100

    Update loading.jsx

--- src/components/loading.jsx
+++ src/components/loading.jsx
@@ -1,9 +1,43 @@
 import React from 'react'
+import styled from 'styled-components'
 
-const loading = () => {
+const Loading = () => {
   return (
-    <div className='loading'></div>
+    <LoadingBar className='loading'>
+        <Span></Span>
+    </LoadingBar>
   )
 }
+const LoadingBar=styled.div`
+    position: relative;
+    display:flex;
+    height:100vh;
+    width:100%;
+    align-items: center;
+    justify-content:center;
 
-export default loading
+`
+const Span=styled.span`
+    @keyframes rotate {
+        to{
+            transform: rotate(360deg);
+        }
+    }
+    position:absolute;
+    display:flex;
+    align-items: center;
+    justify-content: center;
+    height:10rem;
+    width:10rem;
+    border:1.5rem solid #333;
+
+    &::before{
+        content:'';
+        position:absolute;
+        height:1rem;
+        width:1rem;
+        border-radius:50%;
+    }
+`
+export default Loading
+
