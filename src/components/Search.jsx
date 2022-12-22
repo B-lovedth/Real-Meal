809ca1a8c7839b69177a423e88365133ae9680d7
Solomon Great <greatsoma2019@gmail.com>
2022-12-22 11:00:09 +0100

    Update Search.jsx

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,5 +1,5 @@
 import styled from 'styled-components'
-import { useState , useEffect} from 'react'
+import { useState } from 'react'
 import { FaSearch } from 'react-icons/fa'
 import {useNavigate} from 'react-router-dom'
 const Search = () => {
